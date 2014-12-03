3de59a074a4777fe689a6b48ab621d14529574cd
kazuhito-m <kazuhito.sumpic@gmail.com>
2014-12-03 19:17:17 +0900

    conscript/giter8 を使っての新規作成。

---
+++ build.sbt
@@ -0,0 +1,7 @@
+seq(conscriptSettings :_*)
+
+organization := "hm.orz.sumpic"
+
+name := "funiki-crawler"
+
+version := "0.1.0"
---
+++ src/main/scala/App.scala
@@ -0,0 +1,23 @@
+package hm.orz.sumpic
+
+/** The launched conscript entry point */
+class App extends xsbti.AppMain {
+  def run(config: xsbti.AppConfiguration) = {
+    Exit(App.run(config.arguments))
+  }
+}
+
+object App {
+  /** Shared by the launched version and the runnable version,
+   * returns the process status code */
+  def run(args: Array[String]): Int = {
+    println("Hello World: " + args.mkString(" "))
+    0
+  }
+  /** Standard runnable class entrypoint */
+  def main(args: Array[String]) {
+    System.exit(run(args))
+  }
+}
+
+case class Exit(val code: Int) extends xsbti.Exit
