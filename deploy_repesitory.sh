c60a56872b9388c9a7739912ae7f8ab835a7ef87
kazuhito-m <kazuhito.sumpic@gmail.com>
2014-12-05 23:47:53 +0900

    ビルドとデプロイを一撃でやるスクリプトを作成。

---
+++ deploy_repesitory.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# GitHubPagesにしている簡易MavenRepositryにバイナリをアップするスクリプト。
+
+# まずは、バイナリー一式へとビルド。
+sbt clean publish
+
+# Github-Pagesのブランチへ移動。1
+git checkout gh-pages
+
+# 設定により、./target/repo にビルドした一式が入っているはずなので、そこからトップへコピー。
+cp -R ./target/repo/hm ./hm
+
+# git commit
+git commit -a ./ -m 'Build and deploy Binally.'
+git push
+
+# ブランチを元に戻す。
+git checkout master
+
+
